81191abc6e083fb5c049d55470f3c7ea846c299d
pipe.zkk <pipe.zkk@alibaba-inc.com>
2020-10-17 00:59:58 +0800

    Update Code

--- src/dingtalk.js
+++ src/dingtalk.js
@@ -1,15 +1,33 @@
 console.log('index.js');
 const fetch = require('node-fetch');
-const moment = require('moment');
+const moment = require('moment-timezone');
 
-fetch('http://sz.tmsf.com/newhouse/property_33_431137829_price.htm')
-  .then(d => d.text())
-  .then(d => {
-    const arr = d.match(/页数\s+\d+\/(\d+)\s?总数：\s?(\d+)/);
+let total = null;
+let sold = null;
 
-    // 北京时间
-    pushMessage(`date: ${moment().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')}\ntotal: ${arr[2]}`)
-  });
+Promise.all([
+  fetch('http://sz.tmsf.com/newhouse/property_33_431137829_price.htm')
+    .then(d => d.text())
+    .then(d => {
+      const arr = d.match(/页数\s+\d+\/(\d+)\s?总数：\s?(\d+)/);
+      return arr[2];
+    }),
+  fetch(
+    'http://sz.tmsf.com/newhouse/property_33_431137829_price.htm?isopen=1&presellid=&buildingid=&area=&allprice=&housestate=2&housetype=&page='
+  )
+    .then(d => d.text())
+    .then(d => {
+      const arr = d.match(/页数\s+\d+\/(\d+)\s?总数：\s?(\d+)/);
+      return arr[2];
+    }),
+]).then(([total, sold]) => {
+  // 北京时间
+  pushMessage(
+    `date: ${moment()
+      .tz('Asia/Shanghai')
+      .format('YYYY-MM-DD HH:mm:ss')}\ntotal: ${total}\nsold: ${sold}`
+  );
+});
 
 function pushMessage(msg) {
   console.log(`push message to dingtalk: ${msg}`);
