75bb855366ac19d654e78f6f3ee1852f9bb8583b
pipe.zkk <pipe.zkk@alibaba-inc.com>
2021-01-09 15:46:23 +0800

    Update Code

--- src/tmsf/index.js
+++ src/tmsf/index.js
@@ -2,13 +2,15 @@ const sheetV2 = require('../sheet-v2');
 const sellDetail = require('./loupan-sell-detai');
 require('./total');
 
-const LOUPAN_LIST = ['33_430947781', '33_3158256', '33_135092'];
-
+const LOUPAN_LIST = [
+  '33_430947781', // 上悦城
+  '33_3158256', // 嵊星湾
+  '33_135092', // 和雍锦世家
+  '33_1770989', // 铁建观河府
+];
 
 !(async () => {
-
   for (let i = 0; i < LOUPAN_LIST.length; i++) {
-
     try {
       const LOUPAN_ID = LOUPAN_LIST[i];
       const d = new Date();
